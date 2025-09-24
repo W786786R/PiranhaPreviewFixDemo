ae431a690acce32a7290e7888ba502c42a466e3d
Washiur Rahaman <iamwashiurrahaman@gmail.com>
2025-09-23 18:37:03 -0600

    Step 2: Added antiforgery service for preview requests (XSRF-TOKEN)
    Why: Fixes missing anti-forgery tokens needed for preview requests.

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ using Piranha.Data.EF.SQLite;
 using Piranha.Manager.Editor;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add antiforgery service
+builder.Services.AddAntiforgery(options =>
+{
+    options.HeaderName = "XSRF-TOKEN"; // what Piranha preview JS expects
+});
 
 builder.AddPiranha(options =>
 {
-    /**
-     * This will enable automatic reload of .cshtml
-     * without restarting the application. However since
-     * this adds a slight overhead it should not be
-     * enabled in production.
-     */
+    // Hot reload of .cshtml during development
     options.AddRazorRuntimeCompilation = true;
 
     options.UseCms();
@@ -28,23 +28,6 @@ builder.AddPiranha(options =>
     var connectionString = builder.Configuration.GetConnectionString("piranha");
     options.UseEF<SQLiteDb>(db => db.UseSqlite(connectionString));
     options.UseIdentityWithSeed<IdentitySQLiteDb>(db => db.UseSqlite(connectionString));
-
-    /**
-     * Here you can configure the different permissions
-     * that you want to use for securing content in the
-     * application.
-    options.UseSecurity(o =>
-    {
-        o.UsePermission("WebUser", "Web User");
-    });
-     */
-
-    /**
-     * Here you can specify the login url for the front end
-     * application. This does not affect the login url of
-     * the manager interface.
-    options.LoginUrl = "login";
-     */
 });
 
 var app = builder.Build();
@@ -53,6 +36,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+
 
 app.UsePiranha(options =>
 {
